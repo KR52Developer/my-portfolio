e828ec6804e80f39c654cb6801aca6986fb982aa
Kavin R <127727609+KR52Developer@users.noreply.github.com>
2023-11-18 16:24:05 +0530

    Add files via upload

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -13,55 +13,55 @@ const Skills = () => {
 
                 <div className='skills' >
 
-                    <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <FaHtml5 style={{ fontSize: "35px", cursor: 'pointer' }}></FaHtml5>
                         <span>HTML</span>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <FaCss3Alt style={{ fontSize: "35px", cursor: 'pointer' }}></FaCss3Alt>
                         <span>CSS</span>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <IoLogoJavascript style={{ fontSize: "35px", cursor: 'pointer' }} >  </IoLogoJavascript >
                         <span>Javascript</span>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <FaBootstrap style={{ fontSize: "35px", cursor: 'pointer' }}></FaBootstrap>
                         <span>Bootstrap</span>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <FaReact style={{ fontSize: "35px", cursor: 'pointer' }} ></FaReact>
                         <span>React JS</span>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <FaNodeJs style={{ fontSize: "35px", cursor: 'pointer' }} ></FaNodeJs>
                         <span>Node Js</span>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <SiExpress style={{ fontSize: "35px", cursor: 'pointer' }} ></SiExpress >
 
                         <span>Express Js</span>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <SiMongodb style={{ fontSize: "35px", cursor: 'pointer' }} ></SiMongodb >
                         <span>Mongo DB</span>
                     </div >
-                    <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <SiFirebase style={{ fontSize: "35px", cursor: 'pointer' }} ></SiFirebase >
 
                         <span>FireBase</span>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <SiPostman style={{ fontSize: "35px", cursor: 'pointer' }} ></SiPostman >
 
                         <span>PostMan</span>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "column" }}>
-                        <FaWordpress style={{ fontSize: "35px", cursor: 'pointer' }} ></FaWordpress  >
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                        <FaWordpress style={{ fontSize: "35px", cursor: 'pointer', color: '#fe551e' }} ></FaWordpress  >
 
                         <span>Wordpress</span>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <FaGithub style={{ fontSize: "35px", cursor: 'pointer' }}></FaGithub>
                         <span>Git</span>
                     </div>
